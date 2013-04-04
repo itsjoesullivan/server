0986b1ff049315827b19793fc590cd0ea067a637
itsjoesullivan <itsjoesullivan@gmail.com>
2013-04-03 21:25:13 -0400

    more robust everything

--- index.js
+++ index.js
@@ -10,40 +10,61 @@
 
 app.listen(3000);*/
 
+if(typeof module !== 'undefined') {
+	var Socket = require('./test/lib/socket.mock');
+}
 
 
-Req = function(path) {
+
+Req = function(reqData) {
+	if(!reqData) {
+		throw "No request data";
+	}
+	
+	var data = reqData.split('\n');
+	var path = /GET (.*) HTTP\/1\.1/.exec(data[0])[1];
 	this.path = path;
+	
 };
 
 Res = function() {
-	
 };
-Res.prototype.send = function() {
-	
-};
+Res.prototype.send = function() {};
 
-App = function() {};
-App.prototype.get = function(path,cb) {
-	var req = new Req(path);
-	var res = new Res();
-	return cb(req,res);
+App = function() {
+	this.socket = new Socket();
+	this.routes = {};
 };
-App.prototype.routes = [];
+App.prototype.get = function(path,cb) {
+	this.routes[path] = cb;
+};
 App.prototype.handle = function(acceptInfo) {
 	//parse for url
 	// 
-	_(App.prototype.routes).find()
+	for(var i in this.routes) {
+		var route = i;
+		if(acceptInfo === i) {
+			
+			return this.routes[i](acceptInfo);
+		}
+	}
+	return false;
 };
 App.prototype.listen = function(port) {
-
 	var self = this;
-	socket.create("tcp", {}, function(socketInfo) {
-		socket.listen(socketInfo.socketId, "127.0.0.1", port, 20, function(result) {
-			console.log("LISTENING:", result);
-			socket.accept(socketInfo.socketId, self.handle);
+	this.socket.create("tcp", {}, function(socketInfo) {
+		self.socket.listen(socketInfo.socketId, "127.0.0.1", port, 20, function(result) {
+			self.socket.accept(socketInfo.socketId, function() {
+				self.handle.apply(self,arguments);
+			});
 		});
 	});
-
-	
 };
+
+if(typeof module !== 'undefined') {
+	module.exports = {
+		App: App,
+		Req: Req,
+		Res: Res
+	};
+}
